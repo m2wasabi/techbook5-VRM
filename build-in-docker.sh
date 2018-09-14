810e662a810366f7376282255086bd2393d3a0e5
m2wasabi <m2wasabi@gmail.com>
2018-09-15 03:01:34 +0900

    Initialize Book

--- build-in-docker.sh
+++ build-in-docker.sh
@@ -3,4 +3,10 @@
 # コマンド手打ちで作業したい時は以下の通り /book に pwd がマウントされます
 # docker run -i -t -v $(pwd):/book vvakame/review /bin/bash
 
+docker run -i -t -v (""+$Pwd.Path+":/book") vvakame/review /bin/bash
+
+kanji-config-updmap status 
+mktexlsr
+review-pdfmaker config.yml 
+
 docker run -t --rm -v $(pwd):/book vvakame/review:2.4 /bin/bash -ci "cd /book && ./setup.sh && npm run pdf"
